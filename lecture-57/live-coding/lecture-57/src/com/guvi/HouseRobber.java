773e46699f99caf940c179008aca313a147795a3
Nikhilnair48 <nikhil.nair48@gmail.com>
2026-02-14 13:23:32 +0530

    feat: House robber init

---
+++ lecture-57/live-coding/lecture-57/src/com/guvi/HouseRobber.java
@@ -0,0 +1,13 @@
+package com.guvi;
+
+/**
+ ## How to think about it
+ You have houses in a line. Each house has money `nums[i]`.
+ Constraint: if you rob house `i`, you cannot rob `i-1` or `i+1`.
+
+ Goal: maximize total money.
+
+ This screams DP because every choice at `i` depends on what you did at `i-1`.
+*/
+public class HouseRobber {
+}
