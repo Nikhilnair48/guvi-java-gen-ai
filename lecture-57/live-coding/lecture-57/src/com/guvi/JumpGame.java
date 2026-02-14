d61c6a5fcb7f327245e4eb1babda1c51f6e719cc
Nikhilnair48 <nikhil.nair48@gmail.com>
2026-02-14 13:13:48 +0530

    feat: Jump game init

---
+++ lecture-57/live-coding/lecture-57/src/com/guvi/JumpGame.java
@@ -0,0 +1,13 @@
+package com.guvi;
+
+/**
+ ## How to think about it
+
+ You’re at index `0`. Each `nums[i]` tells you the maximum jump length from `i`.
+ Question: Can you reach the last index? (Yes/No)
+
+ This is *not* “minimum jumps” (that’s Jump Game II). Here it’s just reachability.
+
+*/
+public class JumpGame {
+}
