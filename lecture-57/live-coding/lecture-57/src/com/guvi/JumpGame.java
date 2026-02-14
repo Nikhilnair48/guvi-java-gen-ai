abac7abe67b1ee23f70690a4b7cfbdce8e43522a
Nikhilnair48 <nikhil.nair48@gmail.com>
2026-02-14 13:14:36 +0530

    feat: Jump game intuition

--- lecture-57/live-coding/lecture-57/src/com/guvi/JumpGame.java
+++ lecture-57/live-coding/lecture-57/src/com/guvi/JumpGame.java
@@ -8,6 +8,17 @@ package com.guvi;
 
  This is *not* “minimum jumps” (that’s Jump Game II). Here it’s just reachability.
 
+ ## Intuition
+
+ ### Example 1: `[2,3,1,1,4]` -> true
+
+ From 0 you can reach up to index 2. From 1 you can reach up to 4. So last index is reachable.
+
+ ### Example 2: `[3,2,1,0,4]` -> false
+
+ You can get to index 3, but `nums[3]=0` blocks you. You can’t reach 4.
+
+ A good mental model: you’re maintaining a “farthest reachable boundary” as you scan left -> right.
 */
 public class JumpGame {
 }
