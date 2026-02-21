f3202581dbe268e0ebc83a7e079d948ca4d6c395
Nikhilnair48 <nikhil.nair48@gmail.com>
2026-02-21 23:47:49 +0530

    feat: lecture 60 consumer app init

---
+++ lecture-60/live-coding/lecture-60-kafka-intro/src/main/java/com/guvi/ConsumerApp.java
@@ -0,0 +1,11 @@
+package com.guvi;
+
+/**
+ * Our consumer will track the number of enrollments for each course
+ * Java: 4
+ * Spring: 40
+ */
+public class ConsumerApp {
+    public static void main(String[] args) {
+    }
+}
